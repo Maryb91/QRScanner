ba50dca7d51fb1a6a63f9bc71bdfedeb0b012695
Maryb91 <mb.sp91@gmail.com>
2022-08-05 15:03:45 +0100

    Finished the Launch screen animation

--- QRScanner/Controller/LaunchViewController.swift
+++ QRScanner/Controller/LaunchViewController.swift
@@ -8,49 +8,34 @@
 import UIKit
 
 class LaunchViewController: UIViewController {
-
-    //MARK: -Outlets
     
-    @IBOutlet weak var lineLogoLaunch: UIImageView!
+    //MARK: - Variables
+    var animate = animateLine()
+
+    
+    //MARK: -Outlets
+        
     @IBOutlet weak var logoLaunchImage: UIImageView!
+    @IBOutlet weak var lineLogoLaunch: UIImageView!
+    
     
     //MARK: - View Controller
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         lineLogoLaunch.isHidden = true
         self.logoLaunchImage.rotate()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.lineLogoLaunch.isHidden = false
-            self.lineLogoLaunch.layer.add(hoverLine(), forKey: "myHoverAnimation")//call any function
-         }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.2) {
+                self.lineLogoLaunch.isHidden = false
+                self.lineLogoLaunch.layer.add(self.animate.hoverLine(), forKey: "myHoverAnimation")//call any function
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.9) {
+                let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+                let nextViewController = storyBoard.instantiateViewController(withIdentifier: "ScanViewController") as! ScanViewController
+                self.present(nextViewController, animated: true, completion: nil)
+            }
+        }
+      
     }
 }
 
 
-//MARK: - Rotation animation for the logo image at Launch
 
-extension UIView{
-    func rotate() {
-        let rotation : CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
-        rotation.toValue = NSNumber(value: Double.pi * 2)
-        rotation.duration = 1
-        rotation.isCumulative = true
-        rotation.repeatCount = 2
-        self.layer.add(rotation, forKey: "rotationAnimation")
-}
-}
-
-//MARK: - Line animation in Launch Screen
-
-func hoverLine () -> CABasicAnimation
-{
-    let hover = CABasicAnimation(keyPath: "position")
-    hover.isAdditive = true
-    hover.fromValue = NSValue(cgPoint: CGPoint.init(x: 0, y: 15))
-    hover.toValue = NSValue(cgPoint: CGPoint(x: 0.0, y: 110.5))
-    hover.autoreverses = true
-    hover.duration = 2
-    hover.repeatCount = Float.infinity
-    return hover
-}
---
+++ QRScanner/View/animations.swift
@@ -0,0 +1,40 @@
+//
+//  animations.swift
+//  QRScanner
+//
+//  Created by Mariam B on 5/8/2022.
+//
+
+import Foundation
+import UIKit
+
+//MARK: - Rotation animation for the logo image at Launch
+
+extension UIView{
+    func rotate() {
+        let rotation : CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotation.toValue = NSNumber(value: Double.pi * 2)
+        rotation.duration = 1
+        rotation.isCumulative = true
+        rotation.repeatCount = 2
+        self.layer.add(rotation, forKey: "rotationAnimation")
+}
+}
+
+
+//MARK: - Line animation in Launch Screen
+
+class animateLine
+{
+    func hoverLine () -> CABasicAnimation
+{
+    let hover = CABasicAnimation(keyPath: "position")
+    hover.isAdditive = true
+    hover.fromValue = NSValue(cgPoint: CGPoint.init(x: 0, y: 15))
+    hover.toValue = NSValue(cgPoint: CGPoint(x: 0.0, y: 110.5))
+    hover.autoreverses = true
+    hover.duration = 0.5
+    hover.repeatCount = 2
+    return hover
+}
+}
