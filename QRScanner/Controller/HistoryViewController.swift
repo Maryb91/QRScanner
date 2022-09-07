36a5b252e08a3512186825cbb47dd53d575b9755
Mary_b91 <mb.sp91@gmail.com>
2022-09-07 17:52:51 +0100

    Scan button when history is empty (part1)

--- QRScanner/Controller/HistoryViewController.swift
+++ QRScanner/Controller/HistoryViewController.swift
@@ -11,36 +11,49 @@ import RealmSwift
 class HistoryViewController: UITableViewController {
 
     //MARK: - Variables
+    
     var qrCodeDBManager = QRCodeDBManager()
     var qrcodes : Results<QRCode>?
     var qrResultType = QrResultTypes()
-
-    //MARK: - viewSDidLoad
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var scanButton: UIButton!
+    
+    //MARK: - viewDidLoad
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         loadQrCodes()
         self.tableView.rowHeight = 80.0
-
+        scanButton.isHidden = true
     }
-    
-    
+
     //MARK: - TableView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return qrcodes?.count ?? 1
+        if(qrcodes!.count >= 1)
+        {
+            return qrcodes!.count
+        }
+        else{
+            scanButton.isHidden = false
+            scanButton.setTitle("Scan QR codes Now", for: .normal)
+            return 0
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "qrCell", for: indexPath)
-        cell.textLabel?.text = qrcodes?[indexPath.row].type ?? "No qr codes scanned yet"
+        cell.textLabel?.text = qrcodes?[indexPath.row].type
         cell.textLabel?.font = UIFont.systemFont(ofSize: 19.0)
         if qrcodes?[indexPath.row].type == qrCodeTypes.contactType {
             let contactNames = qrResultType.getContactName(result: (qrcodes?[indexPath.row].result)!)
-            cell.detailTextLabel?.text = contactNames ?? "No qr codes scanned yet"
+            cell.detailTextLabel?.text = contactNames
         }
         else {
-        cell.detailTextLabel?.text = qrcodes?[indexPath.row].result ?? "No qr codes scanned yet"
+        cell.detailTextLabel?.text = qrcodes?[indexPath.row].result 
         cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 15.0)
         }
 
@@ -53,5 +66,14 @@ class HistoryViewController: UITableViewController {
         qrcodes = qrCodeDBManager.getHistory()
         tableView.reloadData()
     }
+    
+    //MARK: - Scan function when the Scan button is Pressed
+    
+    @IBAction func scanButtonPressed(_ sender: UIButton) {
+        let storyBoard: UIStoryboard = UIStoryboard(name: StoryBoardIds.storyBoardName, bundle: nil)
+        let newViewController = storyBoard.instantiateViewController(withIdentifier: StoryBoardIds.mainTabID) as! MainTabBarController
+        self.present(newViewController, animated: true, completion: nil)
+    }
+    
 
 }
